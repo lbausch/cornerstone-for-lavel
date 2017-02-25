913863377133ea9df3bf1ee864bb4461f28b5132
Lorenz Bausch <info@lorenzbausch.de>
2017-02-25 17:53:36 +0100

    Use Route instead of $router

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -2,4 +2,4 @@
 
 use Bausch\LaravelCornerstone\Http\Controllers\KeepAliveController;
 
-$router->get('lbausch/laravel-cornerstone/keepalive', KeepAliveController::class.'@keepalive');
+Route::get('lbausch/laravel-cornerstone/keepalive', KeepAliveController::class.'@keepalive');
